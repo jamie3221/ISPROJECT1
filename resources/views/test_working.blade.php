e299c4a6dc62b1967631aec84236327bb9fc6864
jamie3221 <jamie.kariuki@strathmore.edu>
2024-07-15 08:33:34 +0300

    commit

---
+++ resources/views/test_working.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="{{asset('css/test_working.css')}}">
+        <title>Test Working</title>
+    </head>
+    <body>
+        <h1>congrats! It works</h1>
+    </body>
+</html>
